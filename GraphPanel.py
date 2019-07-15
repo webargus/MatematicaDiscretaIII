8ce6da49092d062530b5f8770441097c78541601
bushman_svn <edsonkrop@yahoo.com>
2019-07-15 14:42:14 -0300

    Showing Dijkstra user infos in panel

--- GraphPanel.py
+++ GraphPanel.py
@@ -25,46 +25,57 @@ class GraphPanel:
         header = Frame(wrap)
         header.grid({"row": 0, "column": 0, "sticky": NSEW})
         l1 = Label(header, {"text": "Cálculo do menor percurso pelo algoritmo de Dijkstra",
-                            "font": ("Arial", 12),
-                            "padx": 20,
-                            "pady": 20})
+                            "font": ("Arial", 12)})
         l1.grid({"row": 0, "column": 0})
 
         form = Frame(wrap, {"pady": 8, "padx": 8})
         form.grid({"row": 1, "column": 0, "sticky": NSEW, "pady": 8, "padx": 8})
         form.grid_columnconfigure(0, weight=1)
 
+        text = " Como usar:\n"
+        text += " Criar vértice: clique na área em branco abaixo.\n"
+        text += " Cria aresta: clique nos vértices que quer conectar.\n"
+        text += " Dijkstra: clique no 1º vértice e no 2º com CTRL pressionada."
+        self.info_img = PhotoImage(file="info24.png")
+        Label(form,
+              relief=SUNKEN,
+              text=text,
+              image=self.info_img,
+              compound=LEFT,
+              justify=LEFT,
+              anchor=W).grid(row=0, column=0, sticky=EW, pady=8, columnspan=2)
+
         # result report labels
         self.path = Label(form, anchor=W)
-        self.path.grid(row=0, column=0, sticky=W)
+        self.path.grid(row=1, column=0, sticky=W)
         self.result = Label(form, anchor=W)
-        self.result.grid(row=1, column=0, sticky=W)
+        self.result.grid(row=2, column=0, sticky=W)
 
         # clear btns
         self.reset = Button(form,
-                            width=14,
+                            width=10,
                             command=self._reset_canvas,
                             text="Resetar grafo")
-        self.reset.grid(row=0, column=1, sticky=E, pady=8)
+        self.reset.grid(row=1, column=1, sticky=E, pady=8)
         self.clear = Button(form,
-                            width=14,
+                            width=10,
                             command=self._clear_canvas,
                             text="Excluir grafo")
-        self.clear.grid(row=1, column=1, stick=E)
-
+        self.clear.grid(row=2, column=1, stick=E)
         # infos btn
-        self.info_img = PhotoImage(file="info24.png")
+        """self.info_img = PhotoImage(file="info24.png")
         Button(form,
                image=self.info_img,
                width=24,
                height=24,
                command=GraphPanel._infos,
-               anchor=W).grid(row=2, column=0, sticky=W, pady=8)
+               anchor=W).grid(row=2, column=0, sticky=W, pady=8)"""
 
         canvasF = Frame(wrap, {"relief": SUNKEN, "border": 1})
         canvasF.grid({"pady": 8, "padx": 8, "row": 2, "column": 0, "sticky": NSEW})
         canvasF.grid_columnconfigure(0, weight=1)
         canvasF.grid_rowconfigure(0, weight=1)
+        # create graph canvas passing callback from Dijkstra calculation
         self.canvas = GraphCanvas.GraphCanvas(canvasF, self._do_dijkstra)
 
     def _do_dijkstra(self, sel, graph):
@@ -74,13 +85,6 @@ class GraphPanel:
             s = "Destino inacessível"
         self.path.config(text=("Menor caminho: %s" % s))
         self.result.config(text=("Total percorrido: %.2f" % dist))
-
-    @staticmethod               # user guide msg
-    def _infos():
-        text = "Clique no painel em branco: cria novo vértice\n"
-        text += "Clique em dois vértices em sequência: cria aresta\n"
-        text += "Ctrl + clique em dois vértices em sequência: cálculo Dijkstra"
-        messagebox.showinfo("Infos", text)
 
     def _reset_canvas(self):
         self.path.config(text="")
